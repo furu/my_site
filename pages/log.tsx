504907bdc56c15e969009d09ce127f9bd45bf676
furu <ba09219@gmail.com>
2020-06-30 02:44:47 +0900

    Add <link> for atom feed

--- pages/log.tsx
+++ pages/log.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from "next";
 import Link from "next/link";
+import Head from 'next/head';
 import Layout from "../components/layout";
 import Date from "../components/date";
 import { getSortedPostsData } from "../lib/posts";
@@ -7,6 +8,10 @@ import { getSortedPostsData } from "../lib/posts";
 export default ({ allPostsData }) => {
   return (
     <Layout>
+      <Head>
+        <link href="/log/feed.atom" rel="alternate" title="log - 49.212.143.129" type="application/atom+xml" />
+      </Head>
+
       <header>
         <h1>Log ({allPostsData.length})</h1>
       </header>
